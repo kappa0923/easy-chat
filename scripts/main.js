dd8d82a46292a2ad003ea6d9dc8fc6bf7036272b
kappa0923 <tom.kappa0923@gmail.com>
2018-03-04 14:46:17 +0900

    ServiceWorker用の設定を追加

--- scripts/main.js
+++ scripts/main.js
@@ -192,3 +192,15 @@ window.onload = function () {
   // Initializes EasyChat.
   window.easyChat = new EasyChat();
 };
+
+// Setup ServiceWorker.
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/serviceWorker.js")
+      .then(function(registration) {
+        console.log("serviceWorker registed.");
+      }).catch(function(error) {
+        console.warn("serviceWorker error.", error);
+      });
+  }
+});
